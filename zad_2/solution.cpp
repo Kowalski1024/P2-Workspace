8e21c7d8a2b0deaa7e5dbf466798e30bf9765540
Kowalski1024 <tomasz_wojnar@hotmail.com>
2021-03-12 20:02:17 +0100

    ex 2 classic solution added

--- zad_2/solution.cpp
+++ zad_2/solution.cpp
@@ -14,28 +14,64 @@ struct Employee {
     std::string name = "Jan";
     std::string surname = "Kowalski";
     std::string level = "junior";
-    std::shared_ptr<Document> project;
+    std::shared_ptr <Document> project_smart;
+    Document *project_classic;
 };
 
-int main() {
-    Employee * worker_1 = new Employee;
-    std::shared_ptr<Document> project_nr1(new Document);
-    (*worker_1).project = project_nr1;
+void classic() {
+    Employee *worker_1 = new Employee;
+    Document *project_nr1 = new Document;
+    (*worker_1).project_classic = project_nr1;
     std::cout << "Name: " << worker_1->name << "\nSurname: " << worker_1->surname << std::endl;
-    std::cout << "Programming language: " << worker_1->project->programmingLanguage << std::endl;
+    std::cout << "Programming language: " << worker_1->project_classic->programmingLanguage << std::endl;
 
-    Employee * worker_2 =  new Employee;
+    Employee *worker_2 = new Employee;
     std::cout << "\nNew worker\n" << "Enter name and surname:";
     std::cin >> worker_2->name >> worker_2->surname;
-    std::shared_ptr<Document> project_nr2(new Document);
+    Document *project_nr2 = new Document;
     std::cout << "Enter programming language:";
     std::cin >> project_nr2->programmingLanguage;
     project_nr2->commits = 0;
-    (*worker_2).project = project_nr2;
+    (*worker_2).project_classic = project_nr2;
 
     std::cout << "\nNew worker created\n";
     std::cout << "Name: " << worker_2->name << "\nSurname: " << worker_2->surname << std::endl;
-    std::cout << "Programming language:" << worker_2->project->programmingLanguage << std::endl;
+    std::cout << "Programming language: " << worker_2->project_classic->programmingLanguage << "\n\n";
 
+    delete project_nr1;
+    delete worker_1;
+    delete project_nr2;
+    delete worker_2;
+}
+
+void smartPointers() {
+    Employee *worker_1 = new Employee;
+    std::shared_ptr <Document> project_nr1(new Document);
+    (*worker_1).project_smart = project_nr1;
+    std::cout << "Name: " << worker_1->name << "\nSurname: " << worker_1->surname << std::endl;
+    std::cout << "Programming language: " << worker_1->project_smart->programmingLanguage << std::endl;
+
+    Employee *worker_2 = new Employee;
+    std::cout << "\nNew worker\n" << "Enter name and surname: ";
+    std::cin >> worker_2->name >> worker_2->surname;
+    std::shared_ptr <Document> project_nr2(new Document);
+    std::cout << "Enter programming language: ";
+    std::cin >> project_nr2->programmingLanguage;
+    project_nr2->commits = 0;
+    (*worker_2).project_smart = project_nr2;
+
+    std::cout << "\nNew worker created\n";
+    std::cout << "Name: " << worker_2->name << "\nSurname: " << worker_2->surname << std::endl;
+    std::cout << "Programming language:" << worker_2->project_smart->programmingLanguage << std::endl;
+
+    delete worker_1;
+    delete worker_2;
+}
+
+int main() {
+    std::cout << "Classic structure\n";
+    classic();
+    std::cout << "Structure with smart pointers\n";
+    smartPointers();
     return 0;
 };
