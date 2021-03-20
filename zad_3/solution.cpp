47e354fbadcf500eabd0b69babdbe5e689c524d7
Kowalski1024 <tomasz_wojnar@hotmail.com>
2021-03-20 18:59:50 +0100

    ex 3 regex IPv4 and mask

--- zad_3/solution.cpp
+++ zad_3/solution.cpp
@@ -3,11 +3,12 @@
 #include <regex>
 
 int main() {
-    std::regex IPv4("([0-9]|[1-9][0-9]|1[0-9][0-9]|2[0-4][0-9]|25[0-5])");
+    std::string num = "([0-9]|[1-9][0-9]|1[0-9][0-9]|2[0-4][0-9]|25[0-5])";
+    std::regex IPv4_r("^(" + num + "\\.){3}" + num + "$");
+    std::regex mask_r("^((255\\.){3}"+num+"|((255\\.){2}"+num+".0)|((255\\.)"+num+".0.0)|("+num+".0.0.0))");
     std::string tekst;
-    std::cin >> tekst;
     while(std::cin >> tekst) {
-        if(std::regex_match(tekst, IPv4))
+        if(std::regex_match(tekst, mask_r))
         {
             std::cout << "yes\n";
         } else {
