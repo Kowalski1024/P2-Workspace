83b04bd688b0afb08c84ed7aea5cc4b0e348f36f
Kowalski1024 <tomasz_wojnar@hotmail.com>
2021-03-21 20:40:29 +0100

    ex 3 subnet fix

--- zad_3/solution.cpp
+++ zad_3/solution.cpp
@@ -2,7 +2,7 @@
 #include <string>
 #include <regex>
 
-void Input(int mask[], int ipv4[]) {
+void Input(int address[][4]) {
     std::string ip_num = "([0-9]|[1-9][0-9]|1[0-9][0-9]|2[0-4][0-9]|25[0-5])";
     std::string mask_num = "(255|254|252|248|240|224|192|128|0)";
     std::regex IPv4_r("^(" + ip_num + "\\.){3}" + ip_num + "$");
@@ -19,11 +19,10 @@ void Input(int mask[], int ipv4[]) {
     // ip address to int
     temp = std::strtok(str, ".");
     for(int i=0; i<3; i++) {
-        ipv4[i] = std::atoi(temp);
+        address[0][i] = std::atoi(temp);
         temp = std::strtok(nullptr, ".");
     }
-    ipv4[3] = std::atoi(temp);
-
+    address[0][3] = std::atoi(temp);
 
     do {
         std::cout << "Insert mask\n";
@@ -32,34 +31,28 @@ void Input(int mask[], int ipv4[]) {
 
     temp = std::strtok(str, ".");
     for(int i=0; i<3; i++) {
-        mask[i] = std::atoi(temp);
+        address[1][i] = std::atoi(temp);
         temp = std::strtok(nullptr, ".");
     }
-    mask[3] = std::atoi(temp);
+    address[1][3] = std::atoi(temp);
     delete str;
+
+    for (int i = 0; i < 4; i++) {
+        address[2][i] = (address[0][i] & address[1][i]);
+    }
 }
 
 int main() {
-    int maskInt[4];
-    int ipv4Int[4];
-    int subnet[4];
-    Input(maskInt, ipv4Int);
-    for (int i = 0; i < 4; i++) {
-        subnet[i] = (ipv4Int[i] & maskInt[i]);
+    int address[3][4];
+    Input(address);
+    std::string str[3] = {"IPv4:\n", "Mask:\n", "Subnet:\n"};
+    for(int i=0; i<3; i++) {
+        std::cout << str[i];
+        for(int j=0; j<4; j++) {
+            std::cout << std::bitset<8>(address[i][j]) << ".";
+        }
+        std::cout << std::bitset<8>(address[i][3]) << std::endl;
+        printf("%d.%d.%d.%d\n", address[i][0], address[i][1], address[i][2], address[i][3]);
+        printf("%x.%x.%x.%x\n", address[i][0], address[i][1], address[i][2], address[i][3]);
     }
-    std::cout << "IPv4:\n";
-    std::cout << std::bitset<8>(ipv4Int[0]) << "." << std::bitset<8>(ipv4Int[1]) <<
-    "." << std::bitset<8>(ipv4Int[2]) << "." << std::bitset<8>(ipv4Int[3]) << std::endl;
-    printf("%d.%d.%d.%d\n", ipv4Int[0], ipv4Int[1], ipv4Int[2], ipv4Int[3]);
-    printf("%x.%x.%x.%x\n", ipv4Int[0], ipv4Int[1], ipv4Int[2], ipv4Int[3]);
-    std::cout << "Mask:\n";
-    std::cout << std::bitset<8>(maskInt[0]) << "." << std::bitset<8>(maskInt[1]) <<
-              "." << std::bitset<8>(maskInt[2]) << "." << std::bitset<8>(maskInt[3]) << std::endl;
-    printf("%d.%d.%d.%d\n", maskInt[0], maskInt[1], maskInt[2], maskInt[3]);
-    printf("%x.%x.%x.%x\n", maskInt[0], maskInt[1], maskInt[2], maskInt[3]);
-    std::cout << "Subnet:\n";
-    std::cout << std::bitset<8>(subnet[0]) << "." << std::bitset<8>(subnet[1]) <<
-              "." << std::bitset<8>(subnet[2]) << "." << std::bitset<8>(subnet[3]) << std::endl;
-    printf("%d.%d.%d.%d\n", subnet[0], subnet[1], subnet[2], subnet[3]);
-    printf("%x.%x.%x.%x\n", subnet[0], subnet[1], subnet[2], subnet[3]);
 }
